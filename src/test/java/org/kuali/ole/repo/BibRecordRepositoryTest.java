8ccd3c718b6cab694fc18b97ce6434e582a0ffa1
sheiksalahudeen <sheiksalahudeen.m@kuali.org>
2016-10-27 13:00:02 -0400

    Updated testcase.

--- src/test/java/org/kuali/ole/repo/BibRecordRepositoryTest.java
+++ src/test/java/org/kuali/ole/repo/BibRecordRepositoryTest.java
@@ -1,13 +1,10 @@
 package org.kuali.ole.repo;
 
-import org.apache.commons.collections.CollectionUtils;
 import org.junit.Test;
 import org.kuali.ole.BaseTestCase;
-import org.kuali.ole.model.BibRecord;
 
-import java.util.List;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by sheiks on 27/10/16.
@@ -15,10 +12,11 @@ import static org.junit.Assert.*;
 public class BibRecordRepositoryTest extends BaseTestCase{
 
     @Test
-    public void testFetchAllBib() {
+    public void testFetchCount() {
         assertNotNull(bibRecordRepository);
-        List<BibRecord> allBibs = bibRecordRepository.findAll();
-        assertTrue(CollectionUtils.isNotEmpty(allBibs));
+        long count = bibRecordRepository.count();
+        assertTrue(count != 0);
+        System.out.println("total bib count : " + count);
     }
 
 

0e582ba5e9351e896e0c36ea8ef62f8f0273729f
sheiksalahudeen <sheiksalahudeen.m@kuali.org>
2016-10-27 11:16:36 -0400

    Added spring boot application.

---
+++ src/main/java/org/kuali/ole/OleSolrClientApplication.java
@@ -0,0 +1,12 @@
+package org.kuali.ole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OleSolrClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OleSolrClientApplication.class, args);
+	}
+}
